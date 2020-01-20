8ed39af956178c155fd630cf54cb7059ac3bba1c
git-om <chaithanya.omsai@gmail.com>
2020-01-20 19:15:29 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'keyboard-only-tab-focus';
+  title = 'Keyboard-only Focus';
 }
